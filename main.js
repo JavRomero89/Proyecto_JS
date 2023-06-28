ba189d8e796b0a7192fd2c7a2c39fdb970461fa6
JavRomero89 <javieromero23@gmail.com>
2023-06-28 16:52:09 +0200

    finish

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
-function makeBurger(panInicial, ...ingredientes) {
-    var objetoBurger = {
-      panSuperior: panInicial,
-      panInferior: panInicial,
-      ingredientes: ingredientes
-    };
-  
-    console.log(objetoBurger);
+function makeBurger(pan, ing1, ing2, img3) {
+  const burger = {
+    param1: pan,
+    param2: ing1,
+    param3: ing2,
+    param4: ing3,
+    param5: pan
   }
-  
-  // Ejemplo de uso
-  makeBurger("pan", "lechuga", "tomate", "queso", "cebolla", "salsa");
-  
+  console.log(burger);
+}
+console.log(burger);
+let pan="pan";
+makeBurger("pan", prompt("Elige ingrediente 1"), prompt("Elige ingrediente 2"), prompt("Elige ingrediente 3"))
+
+
+
+function starOperation(patata1,patata2) {
+  let arrayResult = operation(num1,num2);
+console.log(arrayResult[0]);
+alert(`El resultado de la suma de ${num1} + ${num2} = ${arrayResult[0]}`)
+}
